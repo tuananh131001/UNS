f4eef0e2d17e03d826c1187c645af2f32f00fe95
William <s3864077@rmit.edu.vn>
2022-08-07 23:26:46 +0700

    .

--- client/src/components/DeleteForm/DeleteForm.style.js
+++ client/src/components/DeleteForm/DeleteForm.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export const StyledDeleteForm = styled.div`
   position: absolute;
 
-  top: 50% ;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 
